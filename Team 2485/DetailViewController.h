8efc26a12119a39b0602654bb8d23ba8e70b6604
Jeremy McCulloch <Jamsabot@Jeremys-iMac-2.local>
2015-01-11 21:29:08 -0800

    Initial Commit

---
+++ Team 2485/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Team 2485
+//
+//  Created by Jeremy McCulloch on 1/11/15.
+//  Copyright (c) 2015 Jeremy McCulloch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Team 2485/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Team 2485
+//
+//  Created by Jeremy McCulloch on 1/11/15.
+//  Copyright (c) 2015 Jeremy McCulloch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
